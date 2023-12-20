5654474352fbcfac317c1b61702e8f52ba66ac5e
Arman Mukanov <mukanovarman777@gmail.com>
2023-12-20 15:07:05 +0500

    feat: добавил лаунчер

---
+++ Application/GameEngine/GameLauncher.cs
@@ -0,0 +1,70 @@
+﻿using Domain.Entities;
+using Domain.Entities.Challenges;
+using Domain.Entities.Items;
+using Domain.Entities.Locations;
+using Domain.Entities.PlayerEntities;
+
+namespace Application.GameEngine;
+
+public class GameLauncher
+{
+    private Game game;
+
+    public Game Start()
+    {
+        var player = InitializePlayer();
+        var map = InitializeMap();
+
+        var game = new Game(player, map);
+        return game;
+    }
+
+    public Game LoadFromFile(string path)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveProgress(string path)
+    {
+        throw new NotImplementedException();
+    }
+
+    private Game Initialize()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Initialize
+    private Player InitializePlayer()
+    {
+        // генерация р.н. предметов, характеристик
+        var items = new List<Item>();
+        var parameters = new List<Parameter>()
+        {
+            new Parameter(ParameterType.Health, 50),
+            new Parameter(ParameterType.Agility, 50),
+            new Parameter(ParameterType.Luck, 50),
+            new Parameter(ParameterType.Strength, 50),
+        };
+        var player = new Player()
+        {
+            Parameters = parameters,
+            Items = items,
+        };
+        return player;
+    }
+
+    private Map InitializeMap()
+    {
+        var maze =
+            "  ###\n" +
+            "# # #\n" +
+            "#   #\n" +
+            "## ##\n" +
+            "#    ";
+        // maze = "";
+        var mazeChallenge = new MazeChallenge(maze);
+        var location = new Forest(new List<Challenge>() { mazeChallenge });
+        return new Map() { Locations = new List<Location>() { location } };
+    }
+}
