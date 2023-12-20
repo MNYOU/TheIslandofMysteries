3e1555c11ca1ec22458685a98f37f4c6c2fdb4a9
Arman Mukanov <mukanovarman777@gmail.com>
2023-12-20 19:46:04 +0500

    refactor: переделал на использование IContext

---
+++ Application/Context/IViewCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Context;
+
+public interface IViewCommand
+{
+    public char Key { get; }
+
+    public string Title { get; }
+}
---
+++ Application/Context/ViewContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Context;
+
+public class ViewContext: IViewContext
+{
+    public string Title { get; }
+    public IEnumerable<IViewCommand> Commands { get; }
+    
+    public ViewContext(string title, IEnumerable<IViewCommand> commands)
+    {
+        Title = title;
+        Commands = commands;
+    }
+}
